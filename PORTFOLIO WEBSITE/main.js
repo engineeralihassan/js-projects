7714fd0430f066d9b9f40ea991fbc89b134910d6
Ali Hassan <engineralihassan@gmail.com>
2022-06-03 21:22:50 -0700

    Add files via upload

---
+++ PORTFOLIO WEBSITE/main.js
@@ -0,0 +1,28 @@
+var slide_index = 1;  
+displaySlides(slide_index);  
+
+function nextSlide(n) {  
+    displaySlides(slide_index += n);  
+}  
+
+function currentSlide(n) {  
+    displaySlides(slide_index = n);  
+}  
+
+function displaySlides(n) {  
+    var i;  
+    var slides = document.getElementsByClassName("showSlide");  
+    if (n > slides.length) { slide_index = 1 }  
+    if (n < 1) { slide_index = slides.length }  
+    for (i = 0; i < slides.length; i++) {  
+        slides[i].style.display = "none";  
+    }  
+    slides[slide_index - 1].style.display = "block";  
+}
+// ... 
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
